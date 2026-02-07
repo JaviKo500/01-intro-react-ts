fef9aac6e9b0c9d078b276f9fee2613d18272e88
JaViKo500 <javikogutierrez64@gmail.com>
2026-02-07 09:55:24 -0500

    feat: add initial implementation of template strings

---
+++ src/bases/02-template-string.ts
@@ -0,0 +1,7 @@
+const firstName = 'Javier';
+const lastName = "O'neal";
+
+const fullName = `${firstName} ${lastName}:`;
+console.log('<--------------- JK 02-template-string --------------->');
+console.log(lastName + ' ' + lastName + ':' );
+console.log(fullName);
