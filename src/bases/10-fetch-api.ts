b2484b1a781bbf2e9ced5586c78699231b3ae003
JaViKo500 <javikogutierrez64@gmail.com>
2026-02-09 23:54:08 -0500

    feat: add type response gift

--- src/bases/10-fetch-api.ts
+++ src/bases/10-fetch-api.ts
@@ -1,20 +1,21 @@
-const API_KEY = 'JYFliq5Q3HMvcD6QLDTYzXDx6EvzlQfN';
-// 
+import type { Gift, GiphyResponse } from "../data/giphyresponse.data";
 
-const  myRequest = fetch(
+const API_KEY = 'JYFliq5Q3HMvcD6QLDTYzXDx6EvzlQfN';
+
+const myRequest = fetch(
   `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=goku&rating=g`
 );
 
 myRequest.then((response) => response.json())
-.then(({data}) => {
-  const imageUrl = data?.images?.original?.url;
-  console.log(data);
-  console.log(imageUrl);
-  const imageElement = document.createElement('img');
-  imageElement.src = imageUrl;
-  imageElement.alt = data?.slug ?? 'No image found';
-  imageElement.style.width = '50%';
-  document.body.appendChild(imageElement);
-})
-.catch(console.error)
-.finally(() => console.log('continue with my life'));
+  .then(({ data }: GiphyResponse) => createImageComponent(data ??{}))
+  .catch(console.error)
+  .finally(() => console.log('continue with my life'));
+
+  const createImageComponent = ( gift: Gift ) => {
+    const imageUrl = gift?.images?.original?.url;
+    const imageElement = document.createElement('img');
+    imageElement.src = imageUrl ?? '';
+    imageElement.alt = gift?.slug ?? 'No image found';
+    imageElement.style.width = '50%';
+    document.body.appendChild(imageElement);
+  }
