1ac01df590717d9d7dec3bef84f59e5dba53b910
JaViKo500 <javikogutierrez64@gmail.com>
2026-02-09 21:32:12 -0500

    feat: add type return functions

--- src/bases/05-functions.ts
+++ src/bases/05-functions.ts
@@ -2,9 +2,7 @@ function greet(name: string): string {
   return `Hello ${name}!`;
 }
 
-const arrowFunGreet = (name: string): string => {
-  return `Hello ${name}!`;
-}
+const arrowFunGreet = (name: string): string => `Hello ${name}!`;
 
 const message = greet('Javier');
 const message2 = arrowFunGreet('Amy');
@@ -13,7 +11,12 @@ console.log('<--------------- JK 05-functions --------------->');
 console.log(message);
 console.log(message2);
 
-function getUser() {
+interface User {
+  name: string;
+  username: string;
+}
+
+function getUser(): User {
   return {
     name: 'Amy',
     username: 'amy500',
@@ -22,14 +25,16 @@ function getUser() {
 
 const user = getUser();
 
-const getUserArrowFun = () => {
-  return {
-    name: 'Amy',
-    username: 'amy500',
-  }
-}
+const getUserArrowFun = (): User => ({
+  name: 'Amy',
+  username: 'amy500',
+});
 
 const user2 = getUserArrowFun();
 console.log('<--------------- JK 05-functions --------------->');
 console.log(user);
 console.log(user2);
+
+const myNumbers: number[] = [1, 2, 3, 4, 5];
+
+myNumbers.forEach((num: number) => console.log(num));
